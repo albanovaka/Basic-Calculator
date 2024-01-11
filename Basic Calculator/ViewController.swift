158b1f2850fc71df213f5a2a2a7b7864ab420700
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-01-11 10:06:57 -0500

    Initial Commit

---
+++ Basic Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Calculator
+//
+//  Created by Kanykey Albanova on 2024-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
