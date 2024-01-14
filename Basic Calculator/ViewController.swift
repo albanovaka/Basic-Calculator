a6b7776317d12062ca0cf9e9e618132c94b3d638
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-01-14 14:27:06 -0500

    All the Ibactions have been added to the view controller

--- Basic Calculator/ViewController.swift
+++ Basic Calculator/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pourcentageSign(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusMinusSign(_ sender: Any) {
+    }
+    @IBAction func allClearButton(_ sender: Any) {
+    }
+    @IBAction func sevenButton(_ sender: Any) {
+    }
+    @IBAction func eightButton(_ sender: Any) {
+    }
+    
+    @IBAction func nineButton(_ sender: Any) {
+    }
+    @IBAction func multiplyButton(_ sender: Any) {
+    }
+    @IBAction func fourButton(_ sender: Any) {
+    }
+    @IBAction func fiveButton(_ sender: Any) {
+    }
+    @IBAction func sixButton(_ sender: Any) {
+    }
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    @IBAction func oneButton(_ sender: Any) {
+    }
+    @IBAction func twoButton(_ sender: Any) {
+    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+    }
+    @IBAction func plusButton(_ sender: Any) {
+    }
+    
+    @IBAction func zeroButton(_ sender: Any) {
+    }
+    
+    @IBAction func pointButton(_ sender: Any) {
+    }
+    @IBAction func equalButton(_ sender: Any) {
+    }
+    
+    
 }
 
